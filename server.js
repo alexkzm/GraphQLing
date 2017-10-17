471ad0b8d44b7d00efff05f962c34177887b1b5a
alexkzm <alexkzm@gmail.com>
2017-10-16 21:49:27 -0400

    few lines

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const graphqlHTTP = require('graphqlHTTP')
 const app = express()
+
+app.use(/'graphql', graphqlHTTP({
+	graphql: true
+
+}))
 
 app.listen(4000)
