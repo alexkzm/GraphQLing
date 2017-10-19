2fe5274938409f8e2b569673712b182d34fe1620
alexkzm <alexkzm@gmail.com>
2017-10-19 18:22:10 -0400

    adding lines

--- schema.js
+++ schema.js
@@ -1,5 +1,39 @@
 const fetch = require('node-fetch')
 const util = require('util')
 util.promisify(require('xml2js').parseString)
+const {
+	GraphQLSchema,
+	GraphQLObjectType,
+	GraphQLInt,
+	GraphQLString
+}
 
-fetch('').then(response => response.text())
+fetch('')
+.then(response => response.text())
+.then(parseXML)
+
+const AuthorType = new GraphQLObjectType({
+	name: 'Author'
+	description: '...',
+
+	fields: () => ({
+		name: {
+			type: GraphQLString
+		}
+	})
+})
+
+module.exports = new GraphQLSchema({
+	query: new GraphQLObjectType({
+		name: 'Query',
+		description: '...',
+		fields: () => ({
+			author: {
+				type: AuthorType,
+				args: {
+				  id: { type: GraphQLInt }
+				}
+			}
+		})
+	})
+})
