dfe1c8179a1287202dc605845844184aee9a4afa
Alexander K <alexkzm@gmail.com>
2018-06-25 18:31:43 -0400

    front end

---
+++ interface.js
@@ -0,0 +1,20 @@
+const todo = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+        return {
+            id: action.id,
+            text: action.text,
+            completed: false
+        }
+        case 'TOGGLE_TODO':
+         if(state.id === action.id) {
+             return Object.assign({}, state, {
+                 completed: !state.completed
+             })
+         }
+
+         return state;
+        default:
+         return state;
+    }
+} 
--- network.js
+++ network.js
@@ -3,3 +3,7 @@ Object.getOwnPropertyNames(base).map(p => global[p] = base [p])
 const pointEQ = p1 => p2 => p1.x == p2.x && p1.y == p2.y 
 
 const validMove = move => state => state.moves[0].x + move.x != 0 || state.move[0].y + move.y != 0
+
+dropfirst
+
+IDBCursorWithValue()
