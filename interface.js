ac5bac00ad81174ea5b5adf49227a7cad9a2d50d
Alexander K <alexkzm@gmail.com>
2018-06-26 16:42:55 -0400

    redux

--- interface.js
+++ interface.js
@@ -18,3 +18,28 @@ const todo = (state, action) => {
          return state;
     }
 } 
+
+const todos = (state = [], action) => {
+    switch(action.type) {
+      case 'ADD_TODO':
+        return [
+            ...state,
+            todo(underfined, action)
+        ];
+      case 'TOGGLE_TODO':
+        return state.map(t => todo(t, action))
+      default:
+        return state
+    }
+}
+
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+    switch(action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter 
+        default:
+            return state
+    }
+}
+
+const { combineReducers } = Redux
