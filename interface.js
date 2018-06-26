1ee584a52a47f9df610ffdfca62b3a14344204e1
Alexander K <alexkzm@gmail.com>
2018-06-26 17:03:10 -0400

    show

--- interface.js
+++ interface.js
@@ -68,3 +68,28 @@ const FilterButton = ({filter, currentFilter, children}) => {
         }
     }, children)
 }
+
+const getVisibletodos = (todos, filter) => {
+    switch(filter) {
+        case 'SHOW_ALL':
+            return todos;
+        case 'SHOW_ACTIVE':
+            return todos.filter(t => !t.completed);
+        case 'SHOW_COMPLETED':
+            return todos.filter(t => t.completed)
+    }
+}
+
+let nextTodoId = 0
+
+function addTodo(text) {
+    store.dispatch({
+        type: 'ADD_TODO',
+        id: nextTodoId++,
+        text: text
+    })
+}
+
+addTodo('Hello')
+addTodo('Goodbye')
+addTodo('Lucky')
