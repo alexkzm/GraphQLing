26e96cac820e37acd6b2b0a33a5e390e027d48e9
Alexander K <alexkzm@gmail.com>
2018-09-09 11:44:08 -0400

    props

---
+++ hero.js
@@ -0,0 +1,21 @@
+export default () =>
+<div>
+    <p></p>
+    <ul>
+        <li></li>
+        <li></li>
+    </ul>
+</div>
+
+render(){
+
+const agencies = this.props.agencies
+
+return <Layout>
+    <Information>
+
+    </Information>
+</Layout>
+
+
+}
